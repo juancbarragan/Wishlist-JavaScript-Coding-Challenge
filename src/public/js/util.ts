631651cc74a58af5948ba55c07559f41a6a9b548
Juan Carlos Barragan Quintero <juancarlos.barraganquintero@ge.com>
2021-06-18 10:41:21 +1000

    First version flyout

--- src/public/js/util.ts
+++ src/public/js/util.ts
@@ -1,15 +1,48 @@
-const icons = document.querySelectorAll('.fa-heart');
+const icons = document.querySelectorAll('.card-icon');
+const wishListItemContainer = document.querySelector(
+  '.wish-list-item-container'
+);
+const favourites = new Set();
 
 for (let i = 0; i < icons.length; i++) {
   icons[i].addEventListener('click', (e: MouseEvent) => {
     const element = <HTMLElement>e.target;
+    const id = element.id;
+
+    console.log(id);
 
     if (element.classList.contains('selected')) {
-      element.classList.add('selected');
-
       element.classList.remove('selected');
+      favourites.delete(id);
     } else {
+      favourites.add(id);
       element.classList.add('selected');
     }
+
+    const container = <HTMLElement>wishListItemContainer;
+
+    let listOfItems = '';
+
+    console.log(favourites);
+
+    favourites.forEach(item => {
+      listOfItems += '<div><h4>hola</h4></div>';
+    });
+
+    container.innerHTML = listOfItems;
   });
 }
+
+const flyout = document.querySelector('.flyout');
+const flyoutIcon = document.querySelector('.flyout-icon');
+flyoutIcon.addEventListener('click', (e: MouseEvent) => {
+  if (flyout.classList.contains('opened')) {
+    flyout.classList.remove('opened');
+  } else {
+    flyout.classList.add('opened');
+  }
+});
+
+const getItemHtml = (label: string) => {
+  return;
+};
